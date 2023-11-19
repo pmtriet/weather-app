6308a912da32cffebd7c985a57385b16cabddca3
Minh Triet Pham <>
2023-11-19 15:41:01 +0700

    first commit

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Minh Triet Pham on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
