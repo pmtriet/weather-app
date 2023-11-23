b10a742ed3db8b181904af4e371f8475d979dd96
Minh Triet Pham <phamminhtriet0312@gmail.com>
2023-11-23 10:50:55 +0700

    change date time

---
+++ weather-app/weather-app/Hello.swift
@@ -0,0 +1,18 @@
+//
+//  Hello.swift
+//  weather-app
+//
+//  Created by Minh Triet Pham on 23/11/2023.
+//
+
+import SwiftUI
+
+struct Hello: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hello()
+}
